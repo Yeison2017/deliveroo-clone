945fb8604f61bea5763c8aa1dae3908d4bdee405
yeison2017 <yha.big@gmail.com>
2023-02-11 18:31:35 -0500

    Se configura tailwind

--- App.tsx
+++ App.tsx
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello wordlsdfg</Text>
+    <View className="flex-1 items-center justify-center bg-gray-400">
+      <Text className="text-red-500">Hello wordlsdfg</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // content: ["./App.tsx"],
+  // content: ["./App.{js,jsx,ts,tsx}", "./**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

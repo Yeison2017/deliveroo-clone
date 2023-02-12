7c8a475fca83a7856398ef66d20010befb06fc3b
yeison2017 <yha.big@gmail.com>
2023-02-11 22:54:07 -0500

    Se crea el header de la pagina principal

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,43 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { UserIcon, ChevronDownIcon } from "react-native-heroicons/outline";
+// import { Image } from "react-native-svg";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className="pt-5 bg-white">
+      <Text className="text-red-500">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "http://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold text-xl">
+              Current Location33
+              <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+          <UserIcon size={35} color="#00CCBB" />
+        </View>
+      </Text>
+    </SafeAreaView>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // content: ["./App.tsx"],
-  // content: ["./App.{js,jsx,ts,tsx}", "./**/*.{js,jsx,ts,tsx}"],
-  content: ["./App.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
+  // content: ["./App.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
